fe91c848564acad9d02ce1972a4b28edfaae0b40
Installer account on frost <lachele@gmail.com>
2008-07-21 18:38:29 -0400

    Added declaration for load_amber_prmtop. Certainly, there will be other omissions to find... (BLFoley)

--- inc/AMBER/amber_prmtop.h
+++ inc/AMBER/amber_prmtop.h
@@ -12,4 +12,4 @@ void amber_prmtop_init(amber_prmtop *P);
 void read_amber_prmtop_asis(fileset F,amber_prmtop *P);
 assembly parse_amber_prmtop(amber_prmtop *P);
 void find_molecules_molbond_array(int nMB, molbond *MB, int NATOM, molindex *AT);
-
+assembly load_amber_prmtop(fileset F);
