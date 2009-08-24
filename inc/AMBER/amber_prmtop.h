b1a0356a3be9ae8e25f8ffd9b6a1946e565fd354
Lachele Foley <lachele@gmail.com>
2009-08-24 13:05:30 -0400

    Fixed issues related to reading AMBER restart and trajectory files.

--- inc/AMBER/amber_prmtop.h
+++ inc/AMBER/amber_prmtop.h
@@ -18,5 +18,11 @@ void read_amber_prmtop_asis(fileset F,amber_prmtop *P);
 assembly parse_amber_prmtop(amber_prmtop *P);
 void find_molecules_molbond_array(int nMB, molbond *MB, int NATOM, molindex *AT);
 assembly load_amber_prmtop(fileset F);
+void add_trajcrds_to_prmtop_assembly(
+        fileset F, ///< The trajectory file containing the data *Must* be open
+        assembly *A, ///< Assembly ("incoming") to which the crds should be added
+        char ftype, ///< 'c' if coordinate ; 'v' if velocity ; 'r' if restart (might have both)
+        int offset ///< read in the offset-th trajectory -- starts with zero=current
+        );
 
 #endif
